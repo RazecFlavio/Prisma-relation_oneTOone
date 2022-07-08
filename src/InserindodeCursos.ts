4ada675fdcd7a9e09fca4b811dca4f8c1b569eea
RazecFlavio <r4zecflavio@gmail.com>
2022-07-08 20:08:22 -0300

    Criando entidade dentro de entidade

--- src/InserindodeCursos.ts
+++ src/InserindodeCursos.ts
@@ -3,7 +3,17 @@ import { PrismaClient } from '@prisma/client'
 async function main() {
   const prisma = new PrismaClient()
 
-  await prisma.tea
+  await prisma.courses.create({
+    data: {
+      name: 'Curso Prisma ORM',
+      duration: 80,
+      description: 'Curso de orm para stack javascript',
+      teacher: {
+        create: {
+          name: 'Flavio Cezar',
+        },
+      },
+    },
   })
 }
 
--- src/createTeacher.ts
+++ src/createTeacher.ts
@@ -2,6 +2,17 @@ import { PrismaClient } from '@prisma/client'
 
 async function main() {
   const prisma = new PrismaClient()
-  await prisma.courses
+  await prisma.teachers.create({
+    data: {
+      name: 'Professor de ReactJS',
+      course: {
+        create: {
+          name: 'Curso de react',
+          duration: 250,
+          description: 'curso de react, TOP',
+        },
+      },
+    },
+  })
 }
 main()
